998b40531ec3913a0d9565e10bb0b5578db16949
kshakirov <kshakirov@zoral.com.ua>
2016-02-11 19:13:21 +0200

    tests refactored

--- test/test_bearing_spacer.rb
+++ test/test_bearing_spacer.rb
@@ -15,7 +15,7 @@ class TestBearingSpacer < Minitest::Unit::TestCase
   def test_find_all_info
     bearing = BearingSpacer.find('45487')
     assert_equal 0.694000, bearing.outside_dim_min
-  
+
 
     p bearing.part.manfr_part_num
     assert_equal '8-A-2055', bearing.part.manfr_part_num
