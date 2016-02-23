db0c340be85f3fe7a297b8504874bcd77ae305ba
kshakirov <kshakirov@zoral.com.ua>
2016-02-23 15:51:22 +0200

    added config

---
+++ metadata_infrastructure/ruby_dependencies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E
+\curl -sSL https://get.rvm.io | bash -s stable
+source /home/ubuntu/.rvm/scripts/rvm
+rvm install ruby-2.2.3
+apt-get install libmysqlclient-dev
+gem install bundler
+create user 'vagrant'@'localhost' identified by 'vagrant';
+grant all on metadata.* to 'vagrant'@'localhost';
