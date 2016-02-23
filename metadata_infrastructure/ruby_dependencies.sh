96814ed65fabb88670c866e41ae01049934401a7
kshakirov <kshakirov@zoral.com.ua>
2016-02-23 17:55:12 +0200

    almost done

--- metadata_infrastructure/ruby_dependencies.sh
+++ metadata_infrastructure/ruby_dependencies.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E
+
 \curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 rvm install ruby-2.2.3
-apt-get install libmysqlclient-dev
+sudo apt-get install libmysqlclient-dev
 sudo apt-get install libgmp3-dev
 sudo apt-get install imagemagick
 gem install bundler
-mysql -u root  -e "create user 'vagrant'@'localhost' identified by 'vagrant';"
-mysql -u root  -e "grant all on metadata.* to 'vagrant'@'localhost';"
+mysql -u root  -e "grant all on metadata.* to vagrant@localhost identified by 'vagrant'"
