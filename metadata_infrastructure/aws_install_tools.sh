88e367d36c01b07dc290cbbf85b95fa45478835b
kshakirov <kshakirov@zoral.com.ua>
2016-02-24 18:40:09 +0200

    some refactoring 2

--- metadata_infrastructure/aws_install_tools.sh
+++ metadata_infrastructure/aws_install_tools.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+mkdir ~/.aws
+cp aws/* ~/.aws
