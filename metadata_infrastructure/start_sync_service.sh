0c0e8f89c57adff68de94b307ad90f7c49df35d5
kshakirov <kshakirov@zoral.com.ua>
2016-02-26 15:59:13 +0200

    fileserver carefully

--- metadata_infrastructure/start_sync_service.sh
+++ metadata_infrastructure/start_sync_service.sh
@@ -7,3 +7,5 @@ screen -S fileserver
 export METADATA_FILESERVER_DIR=$HOME/fileserver
 export METADATA_IMAGES_COLLECTION=/vagrant/images
 ln -s ~/var/product_images/ images
+
+#carefull about fileserver start it from right folder
