8bf69084c3c02f97778d4f6f061de9afa9123f6d
kshakirov <kshakirov@zoral.com.ua>
2016-03-07 16:11:13 +0200

    test

--- test/test_manfr.rb
+++ test/test_manfr.rb
@@ -6,7 +6,7 @@ class TestManfr < Minitest::Unit::TestCase
   def test_all
     pa = Manfr.first
     assert_equal(1,pa.id)
-    p pa.turbo_type
+    p pa.turbo_type.where('import_pk = ?', 564 )
 
   end
 
