f33b1fc3586e9385fd5bddb0daf4c3f2fc66d144
kshakirov <kshakirov@zoral.com.ua>
2016-03-30 12:43:36 +0300

    WIP #2 universal records

--- lib/src/vwhere_used/where_used_finder.rb
+++ lib/src/vwhere_used/where_used_finder.rb
@@ -2,7 +2,7 @@ class VWhereUsedFinder
   def find_by_id id, page
     per_page = 10
     useds = VWhereUsed.where("principal_id = ?", id).paginate(:page => page, :per_page => per_page)
-    response = {:useds => useds, :current_page  => page, :total_pages => useds.total_pages, :per_page => per_page   }
+    response = {:records => useds, :current_page  => page, :total_pages => useds.total_pages, :per_page => per_page   }
     response.to_json
   end
 end
