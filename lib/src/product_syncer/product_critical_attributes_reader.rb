a6da94b722470c3d0cc7b88b79ce32fb2dc7d389
kshakirov <kshakirov@zoral.com.ua>
2016-06-16 18:39:35 +0300

    fix yaml does not treat good ruby big decimal so it must be explicitly converted to fload let us try

--- lib/src/product_syncer/product_critical_attributes_reader.rb
+++ lib/src/product_syncer/product_critical_attributes_reader.rb
@@ -17,13 +17,26 @@ class ProductCriticalAtttsReader
 
   end
 
+
+  def is_big_decimal_to_f value
+    if value.class.to_s.include? 'BigDecimal'
+      value.to_f
+    else
+      value
+    end
+  end
+
+  def _put_explicit_zero value
+   is_big_decimal_to_f(value) || 0
+  end
+
   def _get_values_from_table table_class_name, attrs, product_id, magento_set_name
     product = Object.const_get(table_class_name).find product_id
     attributes = {}
 
     attrs.each do |attr|
       key_name = attr.idx_name
-      attributes[key_name] = {'value' => product.send(attr.json_name), 'type' => attr.data_type}
+      attributes[key_name] = {'value' => _put_explicit_zero(product.send(attr.json_name)), 'type' => attr.data_type}
     end
     attributes
   end
