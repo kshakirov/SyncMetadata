53e7260f548b7845119ac52eeb2d39a531535c10
kshakirov <kshakirov@zoral.com.ua>
2016-09-01 13:18:15 +0300

    localhost instead of url

--- lib/src/product_syncer/attributes_reader/price_attribute_reader.rb
+++ lib/src/product_syncer/attributes_reader/price_attribute_reader.rb
@@ -1,7 +1,7 @@
 class PriceAttributeReader
 
   def get_attribute id
-    response = JSON.parse(RestClient.get 'metadata.turbointernational.com:8080/magmi/prices', {:params => {:id => id}})
+    response = JSON.parse(RestClient.get 'localhost:8080/magmi/prices', {:params => {:id => id}})
     unless response.nil?
      return response[0] if response.size > 0
     end
